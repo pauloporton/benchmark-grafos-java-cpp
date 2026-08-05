5cf9fca274a928c3920f8afc40c7761ed541df04
Pedro Nascimento <pedro.nascimento.silva.251@ccc.ufcg.edu.br>
2026-08-05 17:06:37 -0300

    Documentação kruskal_java

--- algoritmos/java/Kruskal_Didatico.java
+++ algoritmos/java/Kruskal_Didatico.java
@@ -5,14 +5,23 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ * Implementação do algoritmo de Kruskal.
+ * O objetivo é encontrar a Árvore Geradora Mínima de um grafo.
+ */
 public class Kruskal_Didatico {
+
     public static void main(String[] args) {
+        // Número de vértices do grafo
         int n = 7;
 
+        // Lista de arestas do grafo
         List<Aresta> arestas = new ArrayList<>();
         montaArestas(arestas);
 
+        // Lista que armazenará a árvore geradora mínima
         List<Aresta> arvore = new ArrayList<>();
+
         int p = kruskal(arestas, arvore, n);
 
         if (p != Integer.MAX_VALUE) {
@@ -22,6 +31,7 @@ public class Kruskal_Didatico {
         }
     }
 
+    // Adiciona as arestas do grafo
     public static void montaArestas(List<Aresta> arestas) {
         arestas.add(new Aresta(1, 2, 1));
         arestas.add(new Aresta(1, 5, 3));
@@ -35,23 +45,31 @@ public class Kruskal_Didatico {
         arestas.add(new Aresta(7, 3, 11));
     }
 
+    // Executa o algoritmo de Kruskal
     public static int kruskal(List<Aresta> arestas, List<Aresta> arvore, int n) {
+
+        // Ordena as arestas pelo peso
         Collections.sort(arestas);
 
         Dsu dsu = new Dsu(n);
         int pesoTotal = 0;
 
         for (Aresta a : arestas) {
+
+            // Adiciona a aresta somente se ela não formar ciclo
             if (dsu.uni(a.origem, a.destino)) {
                 arvore.add(a);
                 pesoTotal += a.peso;
-                if (arvore.size() == n-1) {
+
+                // Para quando a árvore estiver completa
+                if (arvore.size() == n - 1) {
                     break;
                 }
             }
         }
 
-        if (arvore.size() == n-1) {
+        // Retorna o peso da árvore ou informa que não foi possível montá-la
+        if (arvore.size() == n - 1) {
             return pesoTotal;
         }
         return Integer.MAX_VALUE;
@@ -59,13 +77,16 @@ public class Kruskal_Didatico {
 }
 
 class Dsu {
+
+    // Vetor que representa os conjuntos
     private int[] pais;
 
     public Dsu(int n) {
-        pais = new int[n+1];
+        pais = new int[n + 1];
         Arrays.fill(pais, -1);
     }
 
+    // Encontra o representante de um conjunto
     public int find(int a) {
         if (pais[a] < 0) {
             return a;
@@ -75,14 +96,17 @@ class Dsu {
         return pais[a];
     }
 
+    // Une dois conjuntos diferentes
     public boolean uni(int a, int b) {
         a = find(a);
         b = find(b);
 
-        if (a == b)  {
+        // Se já pertencem ao mesmo conjunto, forma ciclo
+        if (a == b) {
             return false;
         }
 
+        // União por tamanho
         if (pais[a] < pais[b]) {
             pais[a] += pais[b];
             pais[b] = a;
@@ -90,6 +114,7 @@ class Dsu {
             pais[b] += pais[a];
             pais[a] = b;
         }
+
         return true;
     }
 }
